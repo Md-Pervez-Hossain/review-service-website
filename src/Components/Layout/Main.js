d9e817c47d5ff048a16d3343b778e958594429e8
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2022-11-07 22:27:09 +0600

    initial setup

---
+++ src/Components/Layout/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../Header/Header";
+
+const Main = () => {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+};
+
+export default Main;
