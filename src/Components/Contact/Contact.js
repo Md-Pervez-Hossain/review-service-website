c66b5c2c1dec62f0a5ee2e23e73440b7294d8e65
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2022-11-08 21:28:24 +0600

    contact section add

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,7 +1,66 @@
 import React from "react";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="bg-gray-100 my-16">
+      <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-800 dark:text-gray-100">
+        <div className="flex flex-col justify-between">
+          <div className="space-y-2">
+            <h2 className="text-4xl font-bold text-red-600 leading-tight lg:text-5xl">
+              Let's talk!
+            </h2>
+          </div>
+          <img
+            src="https://purepng.com/public/uploads/large/purepng.com-chefcheftrained-professional-cookfood-preparationkitchenchefsexperienced-1421526669538jccuo.png"
+            alt=""
+            className="p-6 "
+          />
+        </div>
+        <form
+          novalidate=""
+          className="space-y-6 ng-untouched ng-pristine ng-valid"
+        >
+          <div>
+            <label for="name" className="text-sm">
+              Full name
+            </label>
+            <input
+              id="name"
+              type="text"
+              placeholder=""
+              className="w-full p-3 rounded dark:bg-gray-800"
+            />
+          </div>
+          <div>
+            <label for="email" className="text-sm">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="w-full p-3 rounded dark:bg-gray-800"
+            />
+          </div>
+          <div>
+            <label for="message" className="text-sm">
+              Message
+            </label>
+            <textarea
+              id="message"
+              rows="3"
+              className="w-full p-3 rounded dark:bg-gray-800"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="w-full p-3 text-sm font-bold bg-red-600 text-white tracking-wide uppercase rounded dark:bg-violet-400 dark:text-gray-900"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
